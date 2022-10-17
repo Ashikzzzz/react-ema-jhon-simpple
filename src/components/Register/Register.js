f3cc510fb682871e2709b85715ac8fad4609e95e
Ashikzzzz <ashik.emon5.im@gmail.com>
2022-10-18 01:18:27 +0600

    context compo and register compo setup

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Register.css";
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="form-container">
+      <h1 className="form-title">Register Here</h1>
+      <form>
+        <div className="form-control">
+          <label>Email</label>
+          <input type="email" name="email" id="" required />
+        </div>
+        <div className="form-control">
+          <label>Password</label>
+          <input type="password" name="password" id="" required />
+        </div>
+        <div className="form-control">
+          <label>Confirm Password</label>
+          <input type="password" name="confirm password" id="" required />
+        </div>
+        <button className="btn-submit" type="submit">
+          Register
+        </button>
+      </form>
+      <p className="register-text">
+        Already have an account? <Link to="/login">Login here</Link>
+      </p>
     </div>
   );
 };
